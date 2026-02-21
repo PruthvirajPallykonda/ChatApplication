5f8735841ed626d6e8cf6fbea6d42e85c001892f
PRUTHVIRAJ PALLYKONDA <pallikondapruthviraj@outlook.com>
2026-02-21 19:40:48 +0530

    fix server issues

--- chat-frontend/src/main.jsx
+++ chat-frontend/src/main.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter basename="/ChatApplication">
+      <HashRouter>
         <div className="w-full min-h-screen overflow-x-hidden bg-[#0b1220]">
           <App />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   </React.StrictMode>
 );
+
